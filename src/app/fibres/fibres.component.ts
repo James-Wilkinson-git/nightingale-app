878a21b322475006c733511bf0a3a59019e39175
James Wilkinson <78602661+James-Wilkinson-git@users.noreply.github.com>
2024-03-11 09:22:45 -0400

    Sort with signals

--- src/app/fibres/fibres.component.ts
+++ src/app/fibres/fibres.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { ApiService } from '../api-service.service';
 import { CommonModule } from '@angular/common';
 
@@ -12,14 +12,43 @@ import { CommonModule } from '@angular/common';
 export class FibresComponent {
   fibres: any[] = [];
   columns: any[] = [];
+  // Define Signals
+  sortedColumn: WritableSignal<string | null> = signal(null);
+  ascending: WritableSignal<boolean | null> = signal(true);
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     this.apiService.getData('fibres').subscribe((data: any) => {
       this.fibres = data;
-      Object.keys(this.fibres[0]).forEach((key) => {
-        this.columns.push(key);
-      });
+      this.columns = Object.keys(this.fibres[0]);
     });
+  }
+
+  sortBy(column: string) {
+    if (this.sortedColumn() === column) {
+      // Toggle sorting direction by calling the Signal as a function with a new value
+      this.ascending.set(!this.ascending());
+    } else {
+      this.sortedColumn.set(column);
+      this.ascending.set(true);
+    }
+    this.sortData();
+  }
+
+  private sortData() {
+    const direction = this.ascending() ? 1 : -1;
+    const column = this.sortedColumn();
+
+    if (column) {
+      this.fibres.sort((a, b) => {
+        if (a[column] < b[column]) {
+          return -1 * direction;
+        }
+        if (a[column] > b[column]) {
+          return 1 * direction;
+        }
+        return 0;
+      });
+    }
   }
 }
