5fb6ef09194535319912aa263cc971d0c4bf8dfd
James Wilkinson <78602661+James-Wilkinson-git@users.noreply.github.com>
2024-03-11 08:59:53 -0400

    Sorting with no exjs and rxjs

--- src/app/metals/metals.component.ts
+++ src/app/metals/metals.component.ts
@@ -12,6 +12,8 @@ import { CommonModule } from '@angular/common';
 export class MetalsComponent {
   metals: any[] = [];
   columns: any[] = [];
+  selectedColumn: string | null = null;
+  sortDirection: 'asc' | 'desc' = 'asc'; // Default sorting direction
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
@@ -20,6 +22,29 @@ export class MetalsComponent {
       Object.keys(this.metals[0]).forEach((key) => {
         this.columns.push(key);
       });
+    });
+  }
+
+  sortBy(column: string) {
+    if (this.selectedColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.selectedColumn = column;
+      this.sortDirection = 'asc';
+    }
+
+    this.metals.sort((a, b) => {
+      if (this.selectedColumn !== null) {
+        const valueA = a[this.selectedColumn];
+        const valueB = b[this.selectedColumn];
+        if (valueA < valueB) {
+          return this.sortDirection === 'asc' ? -1 : 1;
+        }
+        if (valueA > valueB) {
+          return this.sortDirection === 'asc' ? 1 : -1;
+        }
+      }
+      return 0;
     });
   }
 }
--- src/app/woods/woods.component.ts
+++ src/app/woods/woods.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api-service.service';
 import { CommonModule } from '@angular/common';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-woods',
@@ -10,16 +11,61 @@ import { CommonModule } from '@angular/common';
   styleUrl: './woods.component.scss',
 })
 export class WoodsComponent {
-  woods: any[] = [];
+  woods$: Observable<any[]>;
   columns: any[] = [];
-  constructor(private apiService: ApiService) {}
+  private woodsSubject = new BehaviorSubject<any[]>([]);
+  private selectedColumnSubject = new BehaviorSubject<string | null>(null);
+  private sortDirectionSubject = new BehaviorSubject<'asc' | 'desc'>('asc');
+  constructor(private apiService: ApiService) {
+    this.woods$ = this.woodsSubject.asObservable();
+  }
 
   ngOnInit() {
     this.apiService.getData('woods').subscribe((data: any) => {
-      this.woods = data;
-      Object.keys(this.woods[0]).forEach((key) => {
-        this.columns.push(key);
-      });
+      this.woodsSubject.next(data);
+      this.columns = Object.keys(data[0]);
     });
+  }
+
+  sortBy(column: string) {
+    if (this.selectedColumnSubject.value === column) {
+      this.sortDirectionSubject.next(
+        this.sortDirectionSubject.value === 'asc' ? 'desc' : 'asc'
+      );
+    } else {
+      this.selectedColumnSubject.next(column);
+      this.sortDirectionSubject.next('asc');
+    }
+
+    this.woodsSubject
+      .pipe(map((woods) => this.sortWoods(woods)))
+      .subscribe((sortedWoods) => this.woodsSubject.next(sortedWoods));
+  }
+
+  private sortWoods(woods: any[]): any[] {
+    const { selectedColumn, sortDirection } = this;
+    if (selectedColumn === null) {
+      return woods.slice(); // Return a copy of the original array if no column is selected
+    }
+
+    return woods.slice().sort((a, b) => {
+      const valueA = a[selectedColumn];
+      const valueB = b[selectedColumn];
+      if (valueA < valueB) {
+        return sortDirection === 'asc' ? -1 : 1;
+      }
+      if (valueA > valueB) {
+        return sortDirection === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+  }
+
+  get selectedColumn(): string | null {
+    return this.selectedColumnSubject.value;
+  }
+
+  get sortDirection(): 'asc' | 'desc' {
+    return this.sortDirectionSubject.value;
   }
 }
